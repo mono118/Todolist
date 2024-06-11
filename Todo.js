53a939eb2b5e5d67ca24058a550c98abd6fd50d7
mono118 <mono.chroma118@gmail.com>
2024-06-11 16:55:33 +0900

    リストの形式をtableに変更 / ボタンと期限をリストに追加する処理の追加
    ボタンを押した時の処理は未実装

--- Todo.js
+++ Todo.js
@@ -7,10 +7,22 @@ function TodoAdd(){
     let category = document.getElementById("Todocategory").value;
     let memo = document.getElementById("Todomemo")
 
-    let list = document.createElement("li");
-    list.appendChild(document.createTextNode(name.value));
+    let row = document.createElement("tr");
+    let button = document.createElement("input");
+    button.type = "checkbox";
+    let list = []; //tableに追加する要素を記録していく
+    list.push(button);
+    list.push(document.createTextNode(name.value));
     name.value = ""; //入力ボックスの中身を空にする
-    goingList.appendChild(list);
+    list.push(document.createTextNode(date.value));
+    date.value = "";
+    for (let i = 0; i < list.length; i++) {
+        let data = document.createElement("td");
+        data.appendChild(list[i]);
+        row.appendChild(data);
+    }
+    console.log(row);
+    goingList.appendChild(row);
 }
 
 let addButton = document.getElementById("AddButton");
