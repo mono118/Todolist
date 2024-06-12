c008dea0e37c5e73ac426e5dc670ce5869015fac
mono118 <mono.chroma118@gmail.com>
2024-06-12 15:40:37 +0900

    削除ボタンの実装

--- Todo.js
+++ Todo.js
@@ -8,27 +8,38 @@ function TodoAdd(){
     let memo = document.getElementById("Todomemo")
 
     let row = document.createElement("tr");
-    let button = document.createElement("input");
-    button.type = "checkbox";
-    button.addEventListener("change", () => {
-        if(button.checked){
+
+    let statusButton = document.createElement("input");
+    statusButton.type = "checkbox";
+    statusButton.addEventListener("change", () => {
+        if(statusButton.checked){
             completedList.appendChild(goingList.removeChild(row));
         }else{
             goingList.appendChild(completedList.removeChild(row));
         }
     });
+    let deleteButton = document.createElement("button");
+    deleteButton.innerText = "削除";
+    deleteButton.addEventListener("click", () =>{
+        const deletecheck = window.confirm("削除しますか？");
+        if(deletecheck){
+            row.parentElement.removeChild(row);
+        }
+    });
     let list = []; //tableに追加する要素を記録していく
-    list.push(button);
+    list.push(statusButton);
     list.push(document.createTextNode(name.value));
     name.value = ""; //入力ボックスの中身を空にする
     list.push(document.createTextNode(date.value));
     date.value = "";
+    list.push(deleteButton);
+
     for (let i = 0; i < list.length; i++) {
         let data = document.createElement("td");
         data.appendChild(list[i]);
         row.appendChild(data);
     }
-    console.log(row);
+    //console.log(row);
     goingList.appendChild(row);
 }
 
